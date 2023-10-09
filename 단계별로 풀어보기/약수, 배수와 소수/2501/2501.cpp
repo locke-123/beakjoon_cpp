3f6149e9a17807d78f62239ced123c7d6b2cceb5
locke-123 <113772136+locke-123@users.noreply.github.com>
2023-10-09 22:02:19 +0900

    Update 2501.cpp

--- 단계별로 풀어보기/약수, 배수와 소수/2501/2501.cpp
+++ 단계별로 풀어보기/약수, 배수와 소수/2501/2501.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(){
-    freopen("i.txt", "r", stdin);
+    //freopen("i.txt", "r", stdin);
 
+    int N, K;
+    cin >> N >> K;
+
+    int target = 1;
+    vector<int> arr;
+    while (target <= N)
+    {
+        if(N%target == 0){
+            arr.push_back(target);
+        }
+        if(arr.size() == K){
+            break;
+        }
+        target++;
+    }
+
+    if(arr.size() < K) {
+        cout << 0;
+    } else {
+        cout << arr[K-1];
+    }
+    
 
     return 0;
 }
