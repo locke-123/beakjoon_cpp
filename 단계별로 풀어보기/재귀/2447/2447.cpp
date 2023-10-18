04ca7bc0a38086559d97c527da7e1a1c36b34f60
locke-123 <113772136+locke-123@users.noreply.github.com>
2023-10-19 00:39:48 +0900

    Create 2447.cpp

---
+++ 단계별로 풀어보기/재귀/2447/2447.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void star(int n, int i, int j){
+    if((i/n)%3 == 1 && (j/n)%3 == 1){
+        cout << ' ';
+    } else {
+        if(n/3 == 0){
+            cout << "*";
+        } else {
+            star(n/3, i, j);
+        }
+    }
+}
+
+int main(){
+    //freopen("i.txt", "r", stdin);
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            star(n, i, j);
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
