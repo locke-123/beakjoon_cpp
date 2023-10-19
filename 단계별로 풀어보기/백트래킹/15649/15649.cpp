bf13c2f28f9d94d20740a1fe8b0b1210fe198755
locke-123 <113772136+locke-123@users.noreply.github.com>
2023-10-20 03:22:33 +0900

    Update 15649.cpp

--- 단계별로 풀어보기/백트래킹/15649/15649.cpp
+++ 단계별로 풀어보기/백트래킹/15649/15649.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-vector<int> arr;
-vector<bool> used(10, false);
-int N, M;
-void bt(int num){
-    if(arr.size() == M) {
-        for(int i=0; i<arr.size(); i++){
-            cout << arr[i] << " ";
-        }
-        cout << "\n";
-    } else {
-        for(int i=1; i<=N; i++){
-            if(!used[i]){
-                arr.push_back(i);
-                used[i] = true;
-                bt(num++);
-                arr.pop_back();
-                used[i] = false;
-            }
+void bt(int M, vector<int> &arr, vector<bool> &used, vector<int> &res){
+    if(M == 0) {
+        for(int num:res){
+            cout << num << ' ';
+        } cout << '\n';
+        return;
+    }
+    for(int i=0; i<arr.size(); i++){
+        if(used[i] == false){
+            used[i] = true;
+            res.push_back(arr[i]);
+            bt(M-1, arr, used, res);
+            used[i] = false;
+            res.pop_back();
         }
     }
 }
 
 int main(){
-    freopen("i.txt", "r", stdin);
-
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    //freopen("i.txt", "r", stdin);
+    int N, M;
+    vector<int> arr;
+    vector<int> res;
     cin >> N >> M;
-
-    bt(0);
-
+    for(int i=1; i<=N; i++){
+        arr.push_back(i);
+    }
+    vector<bool> used(arr.size(),false);
+    bt(M, arr, used, res);
     return 0;
 }
